c96db7cbdbf9c30bc2575750d7d96b718919ece4
Robert Nay <rsn.nay@gmail.com>
2015-06-22 12:48:04 -0600

    Update procfile to ngrok 2 syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,20 +4,20 @@ puts 'CREATED ADMIN USER: ' << admin.email
 # Setup default accounts
 if Rails.env.production?
   accounts = [{
-    code: 'openassessments',
-    name: 'Open Assessments',
+    code: 'byuopenassessments',
+    name: 'BYU Open Assessments',
     domain: 'http://www.openassessments.com',
-    lti_key: 'openassessments',
-    lti_secret: '588a0bd03a8fe1c298b667549559334c2aa84a549406457fff37c585039c3d4d717ff2f3fa387da1ba2cb92192630d0003b6a9aace819c40322cd3f4864e0dce',
+    lti_key: 'byuopenassessments',
+    lti_secret: '',
     canvas_uri: 'https://canvas.instructure.com'
   }]
 else
   accounts = [{
-    code: 'openassessmentsdev',
-    name: 'Open Assessments Dev',
+    code: 'byuopenassessmentsdev',
+    name: 'BYU Open Assessments Dev',
     domain: 'http://openassessments.ngrok.com',
-    lti_key: 'openassessments',
-    lti_secret: '0b92c73ea0a8c3bae738317e1777d20bec01662862f67b48a1f626e5d07b10c35fc8daedc4fc6269d6592185fce05320ecc53a999e97e214f2f306ed04135f13',
+    lti_key: 'byuopenassessments',
+    lti_secret: '',
     canvas_uri: 'https://canvas.instructure.com'
   }]
 end
