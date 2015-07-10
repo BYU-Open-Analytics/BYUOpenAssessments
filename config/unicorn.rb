30c2bc68b4b815a2ba73a18a179bef3e951cdcd2
Robert Nay <rsn.nay@gmail.com>
2015-07-10 16:18:20 +0000

    Production config changes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,96 +1,66 @@
-if Rails.env.development?
+
+if ENV["RAILS_ENV"] == "development"
 
   worker_processes 1
   timeout 7200
 
 else
 
-  ###################################################################################
-  #
-  # Use this unicorn configuration for deploying to Heroku
-  #
-  worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-  timeout 15
-  preload_app true
-
-  before_fork do |server, worker|
-   Signal.trap 'TERM' do
-     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-     Process.kill 'QUIT', Process.pid
-   end
-
-   defined?(ActiveRecord::Base) and
-     ActiveRecord::Base.connection.disconnect!
-  end
-
-  after_fork do |server, worker|
-   Signal.trap 'TERM' do
-     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-   end
-
-   defined?(ActiveRecord::Base) and
-     ActiveRecord::Base.establish_connection
-  end
-
-
-
-  ###################################################################################
-  #
   # Use this unicorn configuration for zero downtime deployment to other services (ie AWS)
-  #
   # See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete documentation.
 
-  # env = ENV["RAILS_ENV"] || "development" # Set environment to development unless something else is specified
+   env = ENV["RAILS_ENV"] || "development" # Set environment to development unless something else is specified
+  
+   worker_processes 4
+  
+   preload_app true # Preload our app for more speed
+  
+   timeout 1800 # nuke workers after 30 seconds instead of 60 seconds (the default)
+  
+   pid "/tmp/unicorn.byuopenassessments.pid"
+  
+   if env == "production" || env == 'staging'
+     listen "/tmp/unicorn.byuopenassessments.sock", :backlog => 64
+  
+     # working_directory "/u/apps/byuopenassessments/current"
+     working_directory "/home/ec2-user/BYUOpenAssessments"
 
-  # worker_processes 4
-
-  # preload_app true # Preload our app for more speed
-
-  # timeout 1800 # nuke workers after 30 seconds instead of 60 seconds (the default)
-
-  # pid "/tmp/unicorn.byuopenassessments.pid"
-
-  # if env == "production" || env == 'staging'
-  #   listen "/tmp/unicorn.byuopenassessments.socket", :backlog => 64
-
-  #   working_directory "/u/apps/byuopenassessments/current"
-
-  #   user 'ubuntu', 'ubuntu'
-
-  #   shared_path = "/u/apps/byuopenassessments/shared"# feel free to point this anywhere accessible on the filesystem
-
-  #   stderr_path "/u/apps/byuopenassessments/shared/log/unicorn.stderr.log"
-  #   stdout_path "/u/apps/byuopenassessments/shared/log/unicorn.stdout.log"
-
-  #   before_exec do |server|
-  #     ENV["BUNDLE_GEMFILE"] = "/u/apps/byuopenassessments/current/Gemfile"
-  #   end
-  # end
-
-  # before_fork do |server, worker|
-  #   # The following is highly recomended for Rails + "preload_app true".
-  #   # There's no need for the master process to hold a connection.
-  #   if defined?(ActiveRecord::Base)
-  #     ActiveRecord::Base.connection.disconnect!
-  #   end
-
-  #   # Before forking, kill the master process that belongs to the .oldbin PID.
-  #   # This enables 0 downtime deploys.
-  #   old_pid = "/tmp/unicorn.byuopenassessments.pid.oldbin"
-  #   if File.exists?(old_pid) && server.pid != old_pid
-  #     begin
-  #       Process.kill("QUIT", File.read(old_pid).to_i)
-  #     rescue Errno::ENOENT, Errno::ESRCH
-  #       # someone else did our job for us
-  #     end
-  #   end
-  # end
-
-  # after_fork do |server, worker|
-  #   # The following is *required* for Rails + "preload_app true",
-  #   if defined?(ActiveRecord::Base)
-  #     ActiveRecord::Base.establish_connection
-  #   end
-  # end
+     user 'ec2-user', 'ec2-user'
+  
+     shared_path = "/home/ec2-user/BYUOpenAssessmentsShared"# feel free to point this anywhere accessible on the filesystem
+  
+     stderr_path "/home/ec2-user/BYUOpenAssessmentsShared/log/unicorn.stderr.log"
+     stdout_path "/home/ec2-user/BYUOpenAssessmentsShared/log/unicorn.stdout.log"
+  
+     before_exec do |server|
+       ENV["BUNDLE_GEMFILE"] = "/home/ec2-user/BYUOpenAssessments/Gemfile"
+     end
+   end
+  
+   before_fork do |server, worker|
+     # The following is highly recomended for Rails + "preload_app true".
+     # There's no need for the master process to hold a connection.
+     if defined?(ActiveRecord::Base)
+       ActiveRecord::Base.connection.disconnect!
+     end
+  
+     # Before forking, kill the master process that belongs to the .oldbin PID.
+     # This enables 0 downtime deploys.
+     old_pid = "/tmp/unicorn.byuopenassessments.pid.oldbin"
+     if File.exists?(old_pid) && server.pid != old_pid
+       begin
+         Process.kill("QUIT", File.read(old_pid).to_i)
+       rescue Errno::ENOENT, Errno::ESRCH
+         # someone else did our job for us
+       end
+     end
+   end
+  
+   after_fork do |server, worker|
+     # The following is *required* for Rails + "preload_app true",
+     if defined?(ActiveRecord::Base)
+       ActiveRecord::Base.establish_connection
+     end
+   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150609233431) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
     t.string   "domain"
