bfa291b83774987b1cc49aa08945f650d59030a3
Robert Nay <rsn.nay@gmail.com>
2015-07-13 21:45:46 +0000

    Config tweak for webpack

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.webpack[:use_manifest] = true
+  config.webpack[:use_manifest] = false
 
   # Code is not reloaded between requests.
   config.cache_classes = true
