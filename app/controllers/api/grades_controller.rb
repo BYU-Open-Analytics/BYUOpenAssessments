ef8e10f8d665269200bce8f3fcc7dfd10c9b1f8f
Robert Nay <rsn.nay@gmail.com>
2015-08-21 19:25:11 +0000

    Remove debugger statement

--- app/controllers/api/grades_controller.rb
+++ app/controllers/api/grades_controller.rb
@@ -179,7 +179,6 @@ class Api::GradesController < Api::ApiController
           end
         end
 
-	debugger
         submission_status = (success) ? "Grade posted via LTI successfully." : "There was an error posting the grade: #{res.code_major}."
         if !success
           errors.push("Grade writeback failed.")
