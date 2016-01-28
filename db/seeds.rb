1be6a8263d4f641857067736e09e6e1b61ada4c2
Isaac Pritchett <isaac.pritchett@gmail.com>
2016-01-28 15:37:19 -0700

    Added separate account so that BYU Independent Study could have a unique LTI Key/Secret.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,13 @@ if Rails.env.production?
     lti_key: 'byuopenassessments',
     lti_secret: 'd9d728a27969baea23d57811f83efa9d6a7dbc98bff1b4c0ba8ae6de35a95c5fdce88c69361526a1e0841842d632f27e430457e8b1592b39fa1b5db8039c1f9b',
     canvas_uri: 'https://canvas.instructure.com'
+  },{
+	code: 'isopenassessments',
+	name: 'BYU Independent Study',
+	domain: 'https://ec2-52-26-250-81.us-west-2.compute.amazonaws.com',
+	lti_key: 'isopenassessments',
+	lti_secret:'94e54eb3d075a659e44050587fe6c430f30bf91019df91e9efba2d1c8438b0eeffab9f3d2d37a18921099f628a53e6de5c88e976625f45421558720ca57b4f4f',
+	canvas_uri: 'https://canvas.instructure.com'
   }]
   canvas_uri ||= 'https://canvas.instructure.com'
 else
